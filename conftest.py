c4a5ecb88a10e47805ea9271bd05ac53e847d5e0
sidorov <sidorov@hostco.ru>
2020-10-12 14:58:38 +0500

    config files added

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose browser language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    browser_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+    print("\nquit browser..")
+    # time.sleep(5)
+    browser.quit()
+	
