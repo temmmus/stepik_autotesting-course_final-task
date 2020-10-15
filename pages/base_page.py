6f3c812e23a938554b49b3675dcb4a6c8e4f657b
sidorov <sidorov@hostco.ru>
2020-10-15 18:22:35 +0500

    changed test for product page (added parametrize)

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,7 +5,7 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(10)
+        self.browser.implicitly_wait(3)
     	
     def open(self):   
        self.browser.get(self.url)
